6669e07619abff3f6c000d1d006bcc56d48e36f2
Xabier Pena <Xabi@Xabi-PC>
2012-04-25 17:44:42 +0200

    Mejoradas las alarmas

---
+++ VuelosDroid/src/com/vuelosDroid/backEnd/behind/DatosAlarma.java
@@ -0,0 +1,86 @@
+package com.vuelosDroid.backEnd.behind;
+
+import com.vuelosDroid.backEnd.scrapper.DatosVuelo;
+
+public class DatosAlarma {
+
+	private DatosVuelo datos;
+	private int id;
+	private int sonido;
+	private int aterrizar;
+	private int despegar;
+	private int cambios;
+	private int minutos;
+	
+	public DatosAlarma(DatosVuelo pDatos, int pId){
+		this(pDatos, pId, 0, 0, 0, 0, 0);
+	}
+	
+	public DatosAlarma(DatosVuelo pDatos, int pId, int pSonido, int pAterrizar,
+						int pDespegar, int pCambios,int pMinutos){
+		this.datos = pDatos;
+		this.id = pId;
+		this.sonido = pSonido;
+		this.aterrizar = pAterrizar;
+		this.despegar = pDespegar;
+		this.cambios = pCambios;
+		this.minutos = pMinutos;
+	}
+
+	public DatosVuelo getDatos() {
+		return datos;
+	}
+
+	public void setDatos(DatosVuelo datos) {
+		this.datos = datos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getSonido() {
+		return sonido;
+	}
+
+	public void setSonido(int sonido) {
+		this.sonido = sonido;
+	}
+
+	public int getAterrizar() {
+		return aterrizar;
+	}
+
+	public void setAterrizar(int aterrizar) {
+		this.aterrizar = aterrizar;
+	}
+
+	public int getDespegar() {
+		return despegar;
+	}
+
+	public void setDespegar(int despegar) {
+		this.despegar = despegar;
+	}
+
+	public int getCambios() {
+		return cambios;
+	}
+
+	public void setCambios(int cambios) {
+		this.cambios = cambios;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+	
+}
---
+++ VuelosDroid/src/com/vuelosDroid/backEnd/behind/MiReceiverAntelacion.java
@@ -0,0 +1,78 @@
+package com.vuelosDroid.backEnd.behind;
+
+import com.vuelosDroid.frontEnd.AbstractActivity;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MiReceiverAntelacion extends BroadcastReceiver {
+	
+	Context mContext;
+	private static final String TAG = "VuelosAndroid";
+	private String origen;
+	private String destino;
+	private String hora;
+	private int minutos;
+	private int SIMPLE_NOTFICATION_ID;
+
+	
+	@Override
+    public void onReceive(android.content.Context context, android.content.Intent intent) {
+		Log.i("VuelosAndroid", "MiReceiver - onReceive - recibida la alarma");
+		mContext = context;
+		int flags = intent.getFlags();
+		//String action = intent.getAction();
+		origen = intent.getStringExtra("origen");	
+		destino = intent.getStringExtra("destino");
+		hora = intent.getStringExtra("hora");
+		minutos = intent.getIntExtra("minutos", 0);
+		Log.d("VuelosAndroid", "MiReceiverAntelacion - onReceive - origen: " + origen);
+		Log.d("VuelosAndroid", "MiReceiverAntelacion - onReceive - destino: " + destino);
+		Log.d("VuelosAndroid", "MiReceiverAntelacion - onReceive - hora: " + hora);
+		Log.d("VuelosAndroid", "MiReceiverAntelacion - onReceive - minutos: " + minutos);
+
+		notificar();
+		
+	}
+	
+	public void notificar (){
+		
+		String text = origen.substring(0, origen.indexOf("(")-1);
+		String text2 = destino.substring(0, destino.indexOf("(")-1);
+
+		Context context = mContext;
+		String ns = Context.NOTIFICATION_SERVICE;
+		int icono = android.R.drawable.btn_star_big_on;
+		CharSequence contentTitle = "El vuelo " + text + " - " + destino
+				+ " saldrá en: " + minutos + " minutos";
+		CharSequence contentText = "Hora estimada: " + hora ;
+		long hora = System.currentTimeMillis();
+		
+
+		//Creacion de la notificacion
+		NotificationManager mNotificationManager = (NotificationManager)context.getSystemService(ns);
+		Notification mNotificacion = new Notification(icono, contentTitle, hora);
+
+		//Creacion del intent
+		Intent notIntent = new Intent(context, AbstractActivity.class);
+		PendingIntent contIntent = PendingIntent.getActivity(context, 0, notIntent, 0);
+
+		mNotificacion.setLatestEventInfo(context, contentTitle, contentText, contIntent);
+
+		//AutoCancel: cuando se pulsa la notificaión ésta desaparece
+		mNotificacion.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		//Añadir sonido, vibración y luces
+		mNotificacion.defaults |= Notification.DEFAULT_SOUND;
+		mNotificacion.defaults |= Notification.DEFAULT_VIBRATE;
+		mNotificacion.defaults |= Notification.DEFAULT_LIGHTS;
+		mNotificationManager.notify(SIMPLE_NOTFICATION_ID, mNotificacion);
+
+
+	}
+}
